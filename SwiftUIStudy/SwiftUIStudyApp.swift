342a198d1f214beeb9e594b866c1f0ca7f0527b8
dexiong <dexiong@staff.sina.com.cn>
2022-06-08 09:36:11 +0800

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by dexiong on 2022/6/8.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
